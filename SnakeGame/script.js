2159fdc3baec5164d77820fe78d19a09ff5a3479
FaiqaAamer <aamerfaiqa04@gmail.com>
2026-07-24 01:05:51 +0500

    Added main logic

--- SnakeGame/script.js
+++ SnakeGame/script.js
@@ -13,8 +13,59 @@ let food = {
 document.addEventListener("keydown", changeDirection)
 
 function changeDirection(event){
-    if(event.key === "ArrowLeft" && direction !== "RIGHT") direction === "LEFT"
-    else if(event.key === "ArrowUp" && direction !== "DOWN") direction === "UP"
-    else if(event.key === "ArrowRight" && direction !== "LEFT") direction === "RIGHT"
-    else if(event.key === "ArrowDown" && direction !== "UP") direction === "DOWN"
+    if(event.key === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT"
+    else if(event.key === "ArrowUp" && direction !== "DOWN") direction = "UP"
+    else if(event.key === "ArrowRight" && direction !== "LEFT") direction = "RIGHT"
+    else if(event.key === "ArrowDown" && direction !== "UP") direction = "DOWN"
 }
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    for(let i=0; i<snake.length; i++){
+        ctx.fillStyle = i === 0 ? "green" : "lightgreen";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.fillStyle = "red"
+    ctx.fillRect(food.x, food.y, box, box)
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+    
+    if (direction === "LEFT") snakeX -= box;
+    if (direction === "UP") snakeY -= box;
+    if (direction === "RIGHT") snakeX += box;
+    if (direction === "DOWN") snakeY += box;
+
+    if (snakeX === food.x && snakeY === food.y) {
+        food = {
+        x: Math.floor(Math.random() * 19 + 1) * box,
+        y: Math.floor(Math.random() * 19 + 1) * box
+        };
+    } else {
+        snake.pop();
+    }
+
+    let newHead = { x: snakeX, y: snakeY };
+
+    if (
+        snakeX < 0 || snakeY < 0 || 
+        snakeX >= canvas.width || snakeY >= canvas.height || 
+        collision(newHead, snake)
+    ) {
+        clearInterval(game);
+        alert("Game Over!"); 
+    }
+
+    snake.unshift(newHead);
+    }
+
+    function collision(head, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (head.x === array[i].x && head.y === array[i].y) return true;
+    }
+    return false;
+    }
+
+    let game = setInterval(draw, 100);
